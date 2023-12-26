c018e0b4f04dd1c1415a4cf7dc92aa6c33f1a470
nelson parajuli <nelsonparajuli2064@gmail.com>
2023-12-26 15:22:49 +0545

    trying to show the project as javascirpt it make html so

--- index.js
+++ index.js
@@ -76,3 +76,187 @@ server.listen(3000, () => {
 // server.listen(3000, () => {
 //   console.log('server running at http://localhost:3000');
 // });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const { join } = require('path');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
