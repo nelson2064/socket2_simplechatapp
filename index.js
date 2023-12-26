8400d8b3815815b73eccb5e1772b4cde8009433b
nelson parajuli <nelsonparajuli2064@gmail.com>
2023-12-26 15:38:30 +0545

    room in socket io

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-io.on('connection', function(socket) {
-    socket.on('chat', function(msg) {
-        io.sockets.emit('chat_send', msg);
-    });
-});
+// io.on('connection', function(socket) {
+//     socket.on('chat', function(msg) {
+//         io.sockets.emit('chat_send', msg);
+//     });
+// });
+
+
+
+io.on('connection',function(socket){
+    socket.join('kitchen-room'); //room 1
+    let sizeOfKitchen = io.sockets.adapter.rooms.get("kitchen-room").size; //to calcuate how many user is inside a room
+    io.sockets.in('kitchen-room').emit('cooking',"Fried rice cooking = " + sizeOfKitchen);  
+    io.sockets.in('kitchen-room').emit('boiling',"boiling water");
+
+    socket.join('bed-room'); //room 2
+    io.sockets.in('kitchen-room').emit('sleep',"i am sleeping");
+})
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
