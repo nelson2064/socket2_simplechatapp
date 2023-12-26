d0e644565319174a3ca66c35cd0ee6cc2df3f46a
nelson parajuli <nelsonparajuli2064@gmail.com>
2023-12-26 15:20:29 +0545

    simple chat app completed

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express');
-const { createServer } = require('node:http');
+const { createServer } = require('http');
 const { Server } = require('socket.io');
-const { join } = require('path'); // Add this line
+const { join } = require('path');
 
 const app = express();
-
 const server = createServer(app);
 const io = new Server(server);
 
-
-
-io.on('connection',function(socket){
-   
-    socket.on('chat',function(msg){
-      console.log(msg)
-    })
-})
+io.on('connection', function(socket) {
+    socket.on('chat', function(msg) {
+        io.sockets.emit('chat_send', msg);
+    });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
-
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
