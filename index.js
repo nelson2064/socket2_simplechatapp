1bad4050b35ddf06c78898d29137ce5b1ac0c009
nelson parajuli <nelsonparajuli2064@gmail.com>
2023-12-26 14:47:50 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const { join } = require('path'); // Add this line
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
+
+
+io.on('connection',function(socket){
+    console.log("New user connected")
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
